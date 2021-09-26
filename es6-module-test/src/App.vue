df88489b0fd51002fa715420dca38bb698c94c35
孙隽雄 <jaden@sunjuanxiongdeMacBook-Pro.local>
2021-09-27 00:15:24 +0800

    修复es6仓库问题

---
+++ es6-module-test/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+//import sum from './util'//把sum函数引进来
+import {sum,test} from './util' //静态的，不能放在函数中
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted(){
+    const res=sum(10,20)
+    console.log(res)
+    test()
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ es6-module-test/src/util.js
@@ -0,0 +1,13 @@
+/*function sum(a, b) {
+    return a + b
+}
+
+export default sum*/
+
+export function sum(a, b) {
+    return a + b
+}
+
+export function test() {
+    console.log('test')
+}
